8ed99045b3494f6045dc986b68f07e9629ea608b
Seif302010 <seif302010@gmail.com>
2024-05-03 19:47:47 +0300

    sorting attributes

--- Core/Models/Book.cs
+++ Core/Models/Book.cs
@@ -6,8 +6,7 @@
         public int Id { get; set; }
         [MaxLength(500)]
         public string Title { get; set; } = null!;
-        public int AuthorId { get; set; }
-        public Author? Author { get; set; }
+
         [MaxLength(200)]
         public string Publisher { get; set; } = null!;
         public DateTime PublishingDate { get; set; }
@@ -16,6 +15,8 @@
         public string Hall { get; set; } = null!;
         public bool IsAvailabelForRental { get; set; }
         public string Description { get; set; } = null!;
+        public int AuthorId { get; set; }
+        public Author? Author { get; set; }
         public ICollection<BookCategory> Categories { get; set; } = new List<BookCategory>();
     }
 }
